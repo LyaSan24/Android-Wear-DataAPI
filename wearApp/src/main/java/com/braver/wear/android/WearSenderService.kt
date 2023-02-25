69326ef8ca7afc092960eabbdb0431a65aafea21
Elian66 <elian.souza@icomp.ufam.edu.br>
2023-02-24 23:22:59 -0400

    Permissão de Foreground para Watch

--- wearApp/src/main/java/com/braver/wear/android/WearSenderService.kt
+++ wearApp/src/main/java/com/braver/wear/android/WearSenderService.kt
@@ -4,13 +4,27 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.media.AudioManager
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.RequiresApi
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.DataApi
+import com.google.android.gms.wearable.PutDataMapRequest
+import com.google.android.gms.wearable.Wearable
 import java.util.*
 
 class WearSenderService : Service() {
+
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private val connectionCallbacks =
+        this as com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks
+    private val onConnectionFailed =
+        this as com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
@@ -18,12 +32,21 @@ class WearSenderService : Service() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(connectionCallbacks)
+                .addOnConnectionFailedListener(onConnectionFailed)
+                .build()
+        }
+        mGoogleApiClient!!.connect()
+
         //Call thread
         val timer = Timer(false)
         val timerTask: TimerTask = object : TimerTask() {
             @RequiresApi(Build.VERSION_CODES.Q)
             override fun run() {
-                WearActivity().sendRandomMessageToMobileApp()
+                sendRandomMessageToMobileApp()
             }
         }
         timer.scheduleAtFixedRate(timerTask, 0, 10000)
@@ -42,5 +65,45 @@ class WearSenderService : Service() {
             .setSmallIcon(R.mipmap.app_logo)
         startForeground(1001, notification.build())
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    fun sendRandomMessageToMobileApp() {
+        val wearAvailable = mGoogleApiClient!!.hasConnectedApi(Wearable.API)
+        Log.i("TAG", "----hasConnectedApi------->$wearAvailable")
+        val dataMapRequest = PutDataMapRequest.create(WearActivity.PATH_FOR_MOBILE)
+        val map = dataMapRequest.dataMap
+        map.putString(
+            WearActivity.EXTRA_MESSAGE_FROM_WEAR,
+            getTimestampedDate() + "/" + getVolume(this) + "/" + LocationCollect().getCoordinatesTranslated(
+                this
+            )
+        )
+        map.putLong(WearActivity.EXTRA_CURRENT_TIME, Date().time)
+        val putDataRequest = dataMapRequest.asPutDataRequest()
+        putDataRequest.setUrgent()
+        Wearable.DataApi.putDataItem(mGoogleApiClient!!, putDataRequest)
+            .setResultCallback { dataItemResult: DataApi.DataItemResult ->
+                if (dataItemResult.status.isSuccess) {
+                    Log.i("TAG", "----sendRandomMessageToMobileApp------->Successfully!!")
+                } else {
+                    Log.i("TAG", "----sendRandomMessageToMobileApp------->Failed!!")
+                }
+            }
+    }
+
+    var cal: Calendar = Calendar.getInstance()
+    var hours = cal[Calendar.HOUR_OF_DAY]
+
+    private fun getTimestampedDate(): String {
+        //val date = "$dayNumber-$monthNumber-$year"
+        // time = getTime()
+        //return "$date $time"
+        return hours.toString()
+    }
+
+    private fun getVolume(context: Context?): String {
+        val am = context?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC)
+        return currentVolume.toString()
     }
 }
